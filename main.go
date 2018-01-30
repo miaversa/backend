46ea212aa62e5cd7a551654b9216848d9f0fdf98
tarcisio <sassara.tarcisio@gmail.com>
2018-01-30 20:21:36 -0200

    mod

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/miaversa/backend/router"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"net/http"
 	"os"
 )
@@ -15,6 +17,8 @@ var COMMIT string
 var log = logrus.New()
 
 func main() {
+	viper.SetDefault("license", "apache")
+
 	log.Formatter = &logrus.TextFormatter{}
 	log.Out = os.Stdout
 	log.SetLevel(logrus.DebugLevel)
@@ -23,9 +27,13 @@ func main() {
 	contextLogger.Info("init")
 
 	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.NoCache)
+
 	r.Mount("/carrinho", router.CartRouter())
 	r.Mount("/cliente", router.CustomerRouter())
 	r.Mount("/pagamento", router.PaymentRouter())
+	r.Mount("/assets", router.AssetsRouter())
 
 	log.Error(http.ListenAndServe(":8080", r))
 }
--- router/cart.go
+++ router/cart.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/miaversa/backend/templates"
+	"html/template"
 	"net/http"
 )
 
@@ -12,5 +14,41 @@ func CartRouter() http.Handler {
 }
 
 func cartView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("cart view"))
+
+	cart := Cart{
+		Shipping: 123.1,
+		Items: []CartItem{
+			CartItem{Name: "Anel", Price: 123.10},
+			CartItem{Name: "Colar", Price: 127.11},
+		},
+	}
+
+	t := template.New("cart_view.html")
+	t.Parse(string(templates.MustAsset("cart_view.html")))
+
+	err := t.Execute(w, cart)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Cart struct {
+	Shipping float64
+	Items    []CartItem
+}
+
+type CartItem struct {
+	Name  string
+	Price float64
+}
+
+func (c Cart) Total() float64 {
+	var sum float64 = 0
+	for _, i := range c.Items {
+		sum += i.Price
+	}
+
+	sum += c.Shipping
+
+	return sum
 }
